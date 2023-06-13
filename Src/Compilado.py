fffc8b4042ba65671d7b25e3d7d801144112543d
Eduardo Cervantes García <114730970+Eddspicy@users.noreply.github.com>
2023-06-12 23:36:25 -0600

    Ya compila

--- Src/Compilado.py
+++ Src/Compilado.py
@@ -14,7 +14,7 @@ CONS_010 = "010   NO SE ENCUENTRA END - Error en linea:"
 
 ER_REL = re.compile(r"^(B[CEGHLMNPRSV][ACEILNOQRST])(\s+[\w]{1,256})?(\s*\*\s?[\w|\W]*)?$", flags= re.IGNORECASE)
 ER_INH = re.compile(r"^([ACDFILMNPRSTWX][ABDEGLNOPSTUWXY][ABCDGHILMOPRSTVXY][ABDPSVXY]?)(\s*\*\s?[\w|\W]*)?$", flags= re.IGNORECASE)
-ER_IMM = re.compile(r"([ABCELOS][BDIMNOPRU][ABCDPRSTXY][ABD]?)(\s#)(\d{1,5}|\$[0-9A-F]{2,4}|’[A-Za-z]{1}|%[0-1]{1,16})(\s\*[A-Z]*)?", flags= re.IGNORECASE) #se tiene que cambiar lo de los operandos
+ER_IMM = re.compile(r"^([ABCELOS][BDIMNOPRU][ABCDPRSTXY][ABD]?)(\s*#)(\d{1,5}|\$[0-9A-F]{2,4}||\'\S{1}|\%[0-1]{1,16})(\s*\*\s?[\w|\W]*)?$", flags= re.IGNORECASE) #se tiene que cambiar lo de los operandos
 ER_DIR = re.compile(r"^([ABCELOS][BDIMNOPRU][ABCDPRSTXY][ABD]?[RT]?)(\s){1}(\d{1,3}|^\$[0-9A-F]{2}$|'\S{1}|%[0-1]{1,8}|\w+)(\s*\*\s[A-Z]*)?$", flags= re.IGNORECASE)
 ER_EXT = re.compile(r"^([ABCDEIJLNORST][BDEILMNOPRSTU][ABCDGLMPRSTXY][ABD]?)(\s){1}(\d{1,5}|^\$[0-9A-F]{2,4}$|'\S{1}|%[0-1]{1,16}|\w+)(\s*\*\s[A-Z]*)?$", flags=re.IGNORECASE)
 ER_INDX = re.compile(r"^([ABCDEIJLNORST][BCDEILMNOPRSTU][ABCDEGLMPRSTXY][ABDELRT]?[RT]?)(\s){1}(\d{1,5}|^\$[0-9A-F]{2,4}$|'\S{1}|%[0-1]{1,16}|\w+)(\s*\*\s[A-Z]*)?(,X)(\s\*[A-Z]*)?$", flags= re.IGNORECASE)
@@ -27,10 +27,11 @@ def compilado_ALL5(instruccion, IMM, DIR, EXT, INDX, INDY, stack_compiler_vls, s
         for i in range (len(IMM)):
             if re.fullmatch(acortador_mnemonicos(IMM[i]), grupos[1],  flags= re.IGNORECASE):
                 instruccion = conversor_operandos(instruccion, grupos[3])
+                hex_act = re.split(ER_IMM, instruccion)
                 mnemonico = acortador_opcode(IMM[i])
                 insbytes = acortador_bytes(IMM[i])
-                oprbytes = (len(grupos[3][1:]) / 2)
-                compilado_operandos(instruccion, grupos[3][1:], grupos[5], mnemonico, insbytes, oprbytes, stack_compiler_vls, stack_compiler_s19, stack_compiler_html, stack_error, line, dir_mem)
+                oprbytes = (len(hex_act[3][1:]) / 2)
+                compilado_operandos(instruccion, hex_act[3][1:], grupos[5], mnemonico, insbytes, oprbytes, stack_compiler_vls, stack_compiler_s19, stack_compiler_html, stack_error, line, dir_mem)
 
 def compilado_operandos(instruccion, operando, comentario, mnemonico, insbytes, oprbytes, stack_compiler_vls, stack_compiler_s19, stack_compiler_html, stack_error, line, dir_mem):
     if insbytes == 2:
@@ -60,7 +61,7 @@ def conversor_operandos(instruccion, operando):
     if operando[0] == "$":
         return instruccion
     elif operando[0] == "'":
-        conversion = hex(ord(operando[1:]))
+        conversion = hex(ord(operando[1]))
         instruccion = instruccion.replace(operando, "$"+str(conversion[2:]))
         return instruccion
     elif operando[0] == "%":
--- Src/Pruebas.py
+++ Src/Pruebas.py
@@ -130,11 +130,33 @@ def pruebas():
                 list_labels.append((linea.strip(), hex(0), line))
             elif re.match(r'\s+', linea):
                 line +=1
-                #print("se detecto una instruccion: " +linea)
-                precompilado(linea.strip(), REL, INH, IMM, DIR, EXT, INDX, INDY, stack_compiler_vls, stack_compiler_s19, stack_compiler_html, stack_error, line, list_labels,list_variables, list_constantes, list_comentarios, dir_mem)
             else:
                 line +=1
                 stack_error.append(CONS_009+str(line))
+    
+    line = 0
+    
+    with open("down.ASC","r") as archivo:
+
+        for linea in archivo:
+            
+            linea = linea.rstrip() #se uso porque de alguna manera se tienen espacios, tabulaciones saltos a final de las lineas y las ER no detectan bien por eso
+            if re.fullmatch(r'', linea):
+                line +=1
+            elif re.fullmatch(variables, linea):
+                line +=1
+                list_variables.append((Matcher[1],Matcher[6]))
+            elif re.fullmatch(constantes, linea):
+                line +=1
+            elif re.fullmatch(comentarios, linea):
+                line +=1
+            elif re.fullmatch(etiquetas, linea) and verificar_palabra_reservada(linea):
+                line +=1
+            elif re.match(r'\s+', linea):
+                line +=1
+                precompilado(linea.strip(), REL, INH, IMM, DIR, EXT, INDX, INDY, stack_compiler_vls, stack_compiler_s19, stack_compiler_html, stack_error, line, list_labels,list_variables, list_constantes, list_comentarios, dir_mem)
+            else:
+                line +=1
     
     print("PRUEBA CONSTANTES")
     for i in list_constantes:
